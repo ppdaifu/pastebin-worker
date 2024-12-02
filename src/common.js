f046247551e7a3f07c9606de7e13cc0a61390bd3
ppdaifu <led.habit0o@icloud.com>
2024-12-02 22:24:40 +0800

    Update common.js

--- src/common.js
+++ src/common.js
@@ -2,9 +2,9 @@
 export const params = {
   CHAR_GEN : "abcdef1234567890",
   NAME_REGEX : /^[a-zA-Z0-9+_\-\[\]*$@,;]{3,}$/,
-  RAND_LEN : 32,
-  PRIVATE_RAND_LEN : 6,
-  ADMIN_PATH_LEN : 4,
+  RAND_LEN : 6,
+  PRIVATE_RAND_LEN : 32,
+  ADMIN_PATH_LEN : 2,
   SEP : ":",
   MAX_LEN : 25 * 1024 * 1024,
 }
